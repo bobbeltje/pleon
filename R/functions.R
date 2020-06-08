b594f571bd1573ac6c24131f4007a9adbbf0414b
bobbeltje <matthiaswubs@gmail.com>
2020-06-08 15:19:03 +0200

    frames without colour

--- R/functions.R
+++ R/functions.R
@@ -13,25 +13,37 @@ xaxis <- yaxis <- ax
 xaxis$range <- c(0, 15)
 yaxis$range <- c(-5.5, -0.5)
 
-make_line <- function(chars, line){
+make_line <- function(chars, line, frame=1){
   l <- lapply(chars, function(chr) dat[font == 'cursive' & char == chr])
   for (i in seq_along(l)){
     l[[i]][, 'x' := x + i]
     l[[i]][, 'y' := y - line]
     l[[i]][, 'stroke' := paste(stroke, i, line)]
+    l[[i]][, 'frame' := frame]
   }
   data.table::rbindlist(l)
 }
 
-pleon <- function(...){
-  mc <- match.call(expand.dots = F)
-  x <- unlist(mc$...)
-  char_list <- strsplit(x, '')
-  l <- mapply(make_line, char_list, seq_along(char_list), SIMPLIFY = F)
-  d <- data.table::rbindlist(l)
+pleon <- function(..., l){
+  if (missing(l)){
+    x <- unlist(match.call(expand.dots = F)$...)
+    char_list <- strsplit(x, '')
+    l <- mapply(make_line, char_list, seq_along(char_list), SIMPLIFY = F)
+    d <- data.table::rbindlist(l)
+  }else{
+    pages <- list()
+    for (i in seq_along(l)){
+      char_list <- strsplit(l[[i]], '')
+      l2 <- mapply(make_line, char_list, seq_along(char_list), frame=i, SIMPLIFY=F)
+      pages[[i]] <- data.table::rbindlist(l2)
+    }
+    d <- data.table::rbindlist(pages)
+  }
 
-  plotly::plot_ly(d, x=~x, y=~y, type='scatter', mode='lines', color=~stroke) %>%
+  # split=~stroke not yet working
+  plotly::plot_ly(d, x=~x, y=~y, type='scatter', mode='lines', frame=~frame) %>%
     plotly::layout(showlegend=F, xaxis=xaxis, yaxis=yaxis, margin=list(t=0, b=0, r=0, l=0)) %>%
-    plotly::config(displayModeBar=F)
+    plotly::config(displayModeBar=F) %>%
+    animation_opts(frame = 2000, transition = 800, redraw = F)
 }
 
